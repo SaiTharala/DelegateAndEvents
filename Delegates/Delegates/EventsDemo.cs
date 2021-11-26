14c16ed1a25a36db27b182601ce8772649e0bf5d
unknown <Saitharala@gmail.com>
2021-11-26 21:27:55 +0530

    [ Tharala SaiPrashanth] Added  Delegates And Events Feauture

---
+++ Delegates/Delegates/EventsDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesAndEvents
+{
+    public delegate void DelEventHandler();
+    class EventsDemo
+    {
+        public static void India()
+        {
+            Console.WriteLine("India\n");
+        }
+        public static void UK()
+        {
+            Console.WriteLine("UK\n");
+        }
+        public static void Sydney()
+        {
+            Console.WriteLine("Sydney\n");
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegates Problem");
+            operations obj = new operations(Delegatedemo.Addition);
+            Console.WriteLine("Addition is={0}",obj(22,20));
+        }
+    }
+}
